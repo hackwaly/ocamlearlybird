0684ef4b0e0ddd7c52c1c6ebb30b7d01dd88bc51
文宇祥 <hackwaly@qq.com>
2018-09-28 16:52:42 +0800

    Support debug self

--- ocaml_debug_adapter/main.ml
+++ ocaml_debug_adapter/main.ml
@@ -5,23 +5,37 @@ module Session_set = BatSet.Make (struct
 
 let sessions = ref Session_set.empty
 
-let main () =
-  let addr = Unix.(ADDR_INET (inet_addr_loopback, 4711)) in
-  let%lwt _ = Lwt_io.establish_server_with_client_address addr (
-    fun _ (in_chan, out_chan) ->
-      let session = Session.create in_chan out_chan in
-      sessions := Session_set.add session !sessions;
-      Session.start session;%lwt
-      sessions := Session_set.remove session !sessions;
-      Lwt.return_unit
-  ) in
-  let wait, _ = Lwt.wait () in
-  wait
+let on_connection in_chan out_chan =
+  let session = Session.create in_chan out_chan in
+  sessions := Session_set.add session !sessions;
+  Session.start session;%lwt
+  sessions := Session_set.remove session !sessions;
+  Lwt.return_unit
+
+let at_exit () =
+  Lwt_list.iter_p (fun session ->
+    Session.shutdown session
+  ) (Session_set.to_list !sessions)
+
+let start = function
+  | Some port ->
+    let addr = Unix.ADDR_INET (Unix.inet_addr_loopback, port) in
+    let%lwt _ = Lwt_io.establish_server_with_client_address addr (
+      fun _ (in_chan, out_chan) -> on_connection in_chan out_chan
+    ) in
+    fst (Lwt.wait ())
+  | None -> on_connection Lwt_io.stdin Lwt_io.stdout
+
+let command server (port : int) =
+  let server = if server then Some port else None in
+  Lwt_main.at_exit at_exit;
+  Lwt_main.run (start server)
 
 let () =
-  Lwt_main.at_exit (fun () -> 
-    Lwt_list.iter_p (fun session -> 
-      Session.shutdown session
-    ) (Session_set.to_list !sessions)
-  );
-  Lwt_main.run (main ())
+  let open Cmdliner in
+  let server = Arg.(value & flag & info ["server"]) in
+  let port = Arg.(value & opt int 4711 & info ["port"]) in
+  Term.(exit @@ eval (
+    const command $ server $ port,
+    info "ocamlearlybird"
+  ))
