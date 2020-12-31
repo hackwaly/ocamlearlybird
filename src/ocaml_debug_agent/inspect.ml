ab1bddc06aa2047b305ed0d15c6051b51c6b4b37
文宇祥 <hackwaly@qq.com>
2020-12-31 14:14:05 +0800

    Refactor

--- src/ocaml_debug_agent/inspect.ml
+++ src/ocaml_debug_agent/inspect.ml
@@ -1,7 +1,103 @@
 open Inspect_types
 open Symbols
 
-let get_value find_event conn env rv ty =
+type t = {
+  find_event : Pc.t -> Event.t;
+  find_module : string -> Module.t;
+  lock_conn : 'a. (Debugcom.conn -> 'a Lwt.t) -> 'a Lwt.t;
+  alloc_obj_id : unit -> int;
+  mutable scene : scene option;
+}
+
+let find_obj t id =
+  match t.scene with
+  | None -> raise Not_found
+  | Some scene -> Hashtbl.find scene.obj_tbl id
+
+let exec_with_frame t conn index f =
+  Log.debug (fun m -> m "exec_with_frame");%lwt
+  assert (index >= 0);
+  let rec walk cur (stack_pos, pc) =
+    Log.debug (fun m -> m "exec_with_frame cur:%d" cur);%lwt
+    let ev = t.find_event pc in
+    if cur = index then Lwt.return (Some (stack_pos, pc, ev))
+    else
+      match%lwt Debugcom.up_frame conn ev.ev.Instruct.ev_stacksize with
+      | None -> Lwt.return None
+      | Some (stack_pos, pc) -> walk (cur + 1) (stack_pos, pc)
+  in
+  let%lwt stack_pos, pc = Debugcom.initial_frame conn in
+  let%lwt frame = walk 0 (stack_pos, pc) in
+  (f frame) [%finally Debugcom.set_frame conn stack_pos]
+
+let undef_lazy = Lazy.from_fun (fun () -> assert false)
+
+let rec make_obj t ~name ~value ?(structured = false)
+    ?(list_members = fun _ -> Lwt.return_nil) () =
+  match t.scene with
+  | Some scene ->
+      let id = t.alloc_obj_id () in
+      let obj = { id; name; value; structured; members = undef_lazy } in
+      obj.members <- Lazy.from_fun (fun () -> list_members obj);
+      Hashtbl.replace scene.obj_tbl id obj;
+      obj
+  | None -> assert false
+
+and list_scope_obj t obj =
+  let[@warning "-8"] (Scope { scene_id; index; kind; _ }) =
+    (obj.value [@warning "+8"])
+  in
+  match t.scene with
+  | Some scene when scene.report.rep_event_count = scene_id ->
+      t.lock_conn (fun conn ->
+          let frame = scene.frames.(index) in
+          let event = frame.event in
+          match%lwt Lazy.force frame.env with
+          | exception _ ->
+              Log.debug (fun m -> m "no env");%lwt
+              Lwt.return []
+          | env ->
+              Log.debug (fun m -> m "has env");%lwt
+              let ident_tbl =
+                match kind with
+                | `Stack -> event.ev.ev_compenv.ce_stack
+                | `Heap -> event.ev.ev_compenv.ce_heap
+                | `Global -> assert false
+              in
+              let iter_bindings f =
+                Ident.iter (fun id index -> f (id, index)) ident_tbl
+              in
+              let find_ty env path =
+                let val_desc = env |> Env.find_value path in
+                let ty = Ctype.correct_levels val_desc.Types.val_type in
+                ty
+              in
+              let to_obj (ident, pos) =
+                Log.debug (fun m -> m "local_to_obj: %s" (Ident.name ident));%lwt
+                match find_ty env (Path.Pident ident) with
+                | exception Not_found -> Lwt.return None
+                | ty ->
+                    let%lwt rv =
+                      match kind with
+                      | `Stack ->
+                          Debugcom.get_local conn (event.ev.ev_stacksize - pos)
+                      | `Heap -> Debugcom.get_environment conn pos
+                      | `Global -> [%lwt assert false]
+                    in
+                    let%lwt value = get_value t conn env rv ty in
+                    let obj = make_obj t ~name:(Ident.name ident) ~value () in
+                    Lwt.return (Some obj)
+              in
+              let%lwt objs =
+                exec_with_frame t conn index (fun _ ->
+                    iter_bindings |> Iter.to_list
+                    |> Lwt_list.filter_map_s to_obj)
+              in
+              Log.debug (fun m -> m "list_scope_obj 2");%lwt
+              Lwt.return objs)
+  | _ -> [%lwt assert false]
+
+and get_value t conn env rv ty =
   try%lwt
     if Ctype.matches env Predef.type_int ty then
       let%lwt mv = Debugcom.marshal_obj conn rv in
@@ -23,13 +119,71 @@ let get_value find_event conn env rv ty =
       | Types.Tarrow _ ->
           let%lwt pc = Debugcom.get_closure_code conn rv in
           Log.debug (fun m -> m "%s" (Pc.show pc));%lwt
-          let event = find_event pc in
+          let event = t.find_event pc in
           Lwt.return (Function { location = event.ev.ev_loc })
-      | Tconstr(path, ty_args, _) -> (
-        match env |> Env.find_type path with
-        | _ -> Lwt.return Unknown
-      )
+      | Tconstr (path, ty_args, _) -> (
+          match env |> Env.find_type path with _ -> Lwt.return Unknown )
       | _ -> Lwt.return Unknown
   with ex ->
     Log.debug (fun m -> m "Inspect.get_value error %s" (Printexc.to_string ex));%lwt
     Lwt.return Unknown
+
+let clear_scene t = t.scene <- None
+
+let update_scene t conn report =
+  let obj_tbl = Hashtbl.create 0 in
+  let%lwt frames =
+    match report.Debugcom.rep_type with
+    | Event | Breakpoint ->
+        let%lwt curr_fr_sp, _ = Debugcom.get_frame conn in
+        let make_frame index sp (pc : Pc.t) =
+          let event = t.find_event pc in
+          let module_ = t.find_module event.ev.ev_module in
+          {
+            Frame.index;
+            stack_pos = sp;
+            module_;
+            event;
+            scopes = [];
+            env = event.env;
+          }
+        in
+        let rec walk_up index stacksize frames =
+          let index = index + 1 in
+          match%lwt Debugcom.up_frame conn stacksize with
+          | Some (sp, pc) ->
+              let frame = make_frame index sp pc in
+              walk_up index (Frame.stacksize frame) (frame :: frames)
+          | None -> Lwt.return frames
+        in
+        (let%lwt sp, pc = Debugcom.initial_frame conn in
+         let intial_frame = make_frame 0 sp pc in
+         let%lwt frames =
+           walk_up 0 (Frame.stacksize intial_frame) [ intial_frame ]
+         in
+         let frames = List.rev frames |> Array.of_list in
+         Lwt.return frames)
+          [%finally Debugcom.set_frame conn curr_fr_sp]
+    | _ -> Lwt.return [||]
+  in
+  let scene = { report; frames; obj_tbl } in
+  t.scene <- Some scene;
+  (* We delay set Frame's `scopes` field. Because make_obj need agent's `scene` field to be set *)
+  scene.frames
+  |> Array.iter (fun (frame : Frame.t) ->
+         let make_scope_obj name kind =
+           make_obj t ~name
+             ~value:
+               (Scope
+                  {
+                    scene_id = report.rep_event_count;
+                    index = frame.index;
+                    kind;
+                  })
+             ~structured:true () ~list_members:(list_scope_obj t)
+         in
+         let scopes =
+           [ make_scope_obj "Stack" `Stack; make_scope_obj "Heap" `Heap ]
+         in
+         frame.scopes <- scopes);
+  Lwt.return ()
--- src/ocaml_debug_agent/ocaml_debug_agent.ml
+++ src/ocaml_debug_agent/ocaml_debug_agent.ml
@@ -42,37 +42,12 @@ type t = {
   (* #endregion *)
   breakpoints : Breakpoints.t;
   mutable pendings : (Debugcom.conn -> unit Lwt.t) list;
-  alloc_obj_id : unit -> int;
-  mutable scene : scene option;
+  mutable inspect : Inspect.t;
 }
 
 module Module = Module
 module Event = Event
 module Frame = Frame
-
-let create options =
-  let status_s, set_status = React.S.create Entry in
-  let action_e, emit_action = Lwt_react.E.create () in
-  let symbols_updated_e, emit_symbols_updated = Lwt_react.E.create () in
-  let breakpoints = Breakpoints.create () in
-  {
-    options;
-    status_s;
-    set_status;
-    action_e;
-    emit_action;
-    event_by_pc = Hashtbl.create 0;
-    events_commit_queue = Hashtbl.create 0;
-    events_committed = Hashtbl.create 0;
-    module_by_id = Hashtbl.create 0;
-    module_by_source = Hashtbl.create 0;
-    symbols_updated_e;
-    emit_symbols_updated;
-    breakpoints;
-    pendings = [];
-    alloc_obj_id = Unique_id.make_alloc ();
-    scene = None;
-  }
 
 let symbols_updated_event agent = agent.symbols_updated_e
 
@@ -91,6 +66,62 @@ let is_running agent =
   match agent.status_s |> Lwt_react.S.value with Running -> true | _ -> false
 
 let status_signal agent = agent.status_s
+
+let exec_in_loop agent f =
+  Log.debug (fun m -> m "exec_in_loop 1");%lwt
+  let promise, resolver = Lwt.task () in
+  agent.pendings <-
+    (fun conn ->
+      Log.debug (fun m -> m "exec_in_loop 2");%lwt
+      match%lwt f conn with
+      | result ->
+          Lwt.wakeup_later resolver result;
+          Lwt.return ()
+      | exception exc ->
+          Lwt.wakeup_later_exn resolver exc;
+          Lwt.return ())
+    :: agent.pendings;
+  agent.emit_action `Wakeup;
+  Log.debug (fun m -> m "exec_in_loop 3");%lwt
+  promise
+
+let stack_frames agent =
+  Lwt.return (match agent.inspect.scene with None -> [||] | Some scene -> scene.frames)
+
+let find_obj agent id =
+  Inspect.find_obj agent.inspect id
+
+let create options =
+  let status_s, set_status = React.S.create Entry in
+  let action_e, emit_action = Lwt_react.E.create () in
+  let symbols_updated_e, emit_symbols_updated = Lwt_react.E.create () in
+  let breakpoints = Breakpoints.create () in
+  let agent = {
+    options;
+    status_s;
+    set_status;
+    action_e;
+    emit_action;
+    event_by_pc = Hashtbl.create 0;
+    events_commit_queue = Hashtbl.create 0;
+    events_committed = Hashtbl.create 0;
+    module_by_id = Hashtbl.create 0;
+    module_by_source = Hashtbl.create 0;
+    symbols_updated_e;
+    emit_symbols_updated;
+    breakpoints;
+    pendings = [];
+    inspect = Obj.magic ();
+  }
+  in
+  agent.inspect <- {
+    find_event = find_event agent;
+    find_module = find_module agent;
+    lock_conn = (fun f -> exec_in_loop agent f);
+    alloc_obj_id = Unique_id.make_alloc ();
+    scene = None;
+  };
+  agent
 
 let set_breakpoint agent pc =
   Breakpoints.set agent.breakpoints pc;
@@ -111,119 +142,6 @@ let step_over agent = agent.emit_action `Step_over
 let pause agent = agent.emit_action `Pause
 
 let stop agent = agent.emit_action `Stop
-
-let stack_frames agent =
-  Lwt.return (match agent.scene with None -> [||] | Some scene -> scene.frames)
-
-let exec_in_loop agent f =
-  Log.debug (fun m -> m "exec_in_loop 1");%lwt
-  let promise, resolver = Lwt.task () in
-  agent.pendings <-
-    (fun conn ->
-      Log.debug (fun m -> m "exec_in_loop 2");%lwt
-      match%lwt f conn with
-      | result ->
-          Lwt.wakeup_later resolver result;
-          Lwt.return ()
-      | exception exc ->
-          Lwt.wakeup_later_exn resolver exc;
-          Lwt.return ())
-    :: agent.pendings;
-  agent.emit_action `Wakeup;
-  Log.debug (fun m -> m "exec_in_loop 3");%lwt
-  promise
-
-let find_obj agent id =
-  match agent.scene with
-  | None -> raise Not_found
-  | Some scene -> Hashtbl.find scene.obj_tbl id
-
-let exec_with_frame agent conn index f =
-  Log.debug (fun m -> m "exec_with_frame");%lwt
-  assert (index >= 0);
-  let rec walk cur (stack_pos, pc) =
-    Log.debug (fun m -> m "exec_with_frame cur:%d" cur);%lwt
-    let ev = find_event agent pc in
-    if cur = index then Lwt.return (Some (stack_pos, pc, ev))
-    else
-      match%lwt Debugcom.up_frame conn ev.ev.Instruct.ev_stacksize with
-      | None -> Lwt.return None
-      | Some (stack_pos, pc) -> walk (cur + 1) (stack_pos, pc)
-  in
-  let%lwt stack_pos, pc = Debugcom.initial_frame conn in
-  let%lwt frame = walk 0 (stack_pos, pc) in
-  (f frame) [%finally Debugcom.set_frame conn stack_pos]
-
-let undef_lazy = Lazy.from_fun (fun () -> assert false)
-
-let rec make_obj agent ~name ~value ?(structured = false)
-    ?(list_members = fun _ -> Lwt.return_nil) () =
-  match agent.scene with
-  | Some scene ->
-      let id = agent.alloc_obj_id () in
-      let obj = { id; name; value; structured; members = undef_lazy } in
-      obj.members <- Lazy.from_fun (fun () -> list_members obj);
-      Hashtbl.replace scene.obj_tbl id obj;
-      obj
-  | None -> assert false
-
-and list_scope_obj agent obj =
-  let[@warning "-8"] (Scope { scene_id; index; kind; _ }) =
-    (obj.value [@warning "+8"])
-  in
-  match agent.scene with
-  | Some scene when scene.report.rep_event_count = scene_id ->
-      exec_in_loop agent (fun conn ->
-          let frame = scene.frames.(index) in
-          let event = frame.event in
-          match%lwt Lazy.force frame.env with
-          | exception _ ->
-              Log.debug (fun m -> m "no env");%lwt
-              Lwt.return []
-          | env ->
-              Log.debug (fun m -> m "has env");%lwt
-              let ident_tbl =
-                match kind with
-                | `Stack -> event.ev.ev_compenv.ce_stack
-                | `Heap -> event.ev.ev_compenv.ce_heap
-                | `Global -> assert false
-              in
-              let iter_bindings f =
-                Ident.iter (fun id index -> f (id, index)) ident_tbl
-              in
-              let find_ty env path =
-                let val_desc = env |> Env.find_value path in
-                let ty = Ctype.correct_levels val_desc.Types.val_type in
-                ty
-              in
-              let to_obj (ident, pos) =
-                Log.debug (fun m -> m "local_to_obj: %s" (Ident.name ident));%lwt
-                match find_ty env (Path.Pident ident) with
-                | exception Not_found -> Lwt.return None
-                | ty ->
-                    let%lwt rv =
-                      match kind with
-                      | `Stack ->
-                          Debugcom.get_local conn (event.ev.ev_stacksize - pos)
-                      | `Heap -> Debugcom.get_environment conn pos
-                      | `Global -> [%lwt assert false]
-                    in
-                    let%lwt value =
-                      Inspect.get_value (find_event agent) conn env rv ty
-                    in
-                    let obj =
-                      make_obj agent ~name:(Ident.name ident) ~value ()
-                    in
-                    Lwt.return (Some obj)
-              in
-              let%lwt objs =
-                exec_with_frame agent conn index (fun _ ->
-                    iter_bindings |> Iter.to_list
-                    |> Lwt_list.filter_map_s to_obj)
-              in
-              Log.debug (fun m -> m "list_scope_obj 2");%lwt
-              Lwt.return objs)
-  | _ -> [%lwt assert false]
 
 let load_debug_info frag agent file =
   let%lwt modules = Symbols.load frag file in
@@ -293,65 +211,6 @@ let start agent =
     Log.debug (fun m -> m "wait_action 2");%lwt
     Lwt.return action
   in
-  let clear_scene agent = agent.scene <- None in
-  let update_scene agent report =
-    let obj_tbl = Hashtbl.create 0 in
-    let%lwt frames =
-      match report.Debugcom.rep_type with
-      | Event | Breakpoint ->
-          let%lwt curr_fr_sp, _ = Debugcom.get_frame conn in
-          let make_frame index sp (pc : pc) =
-            let event = find_event agent pc in
-            let module_ = find_module agent event.ev.ev_module in
-            {
-              Frame.index;
-              stack_pos = sp;
-              module_;
-              event;
-              scopes = [];
-              env = event.env;
-            }
-          in
-          let rec walk_up index stacksize frames =
-            let index = index + 1 in
-            match%lwt Debugcom.up_frame conn stacksize with
-            | Some (sp, pc) ->
-                let frame = make_frame index sp pc in
-                walk_up index (Frame.stacksize frame) (frame :: frames)
-            | None -> Lwt.return frames
-          in
-          (let%lwt sp, pc = Debugcom.initial_frame conn in
-           let intial_frame = make_frame 0 sp pc in
-           let%lwt frames =
-             walk_up 0 (Frame.stacksize intial_frame) [ intial_frame ]
-           in
-           let frames = List.rev frames |> Array.of_list in
-           Lwt.return frames)
-            [%finally Debugcom.set_frame conn curr_fr_sp]
-      | _ -> Lwt.return [||]
-    in
-    let scene = { report; frames; obj_tbl } in
-    agent.scene <- Some scene;
-    (* We delay set Frame's `scopes` field. Because make_obj need agent's `scene` field to be set *)
-    scene.frames
-    |> Array.iter (fun (frame : Frame.t) ->
-           let make_scope_obj name kind =
-             make_obj agent ~name
-               ~value:
-                 (Scope
-                    {
-                      scene_id = report.rep_event_count;
-                      index = frame.index;
-                      kind;
-                    })
-               ~structured:true () ~list_members:(list_scope_obj agent)
-           in
-           let scopes =
-             [ make_scope_obj "Stack" `Stack; make_scope_obj "Heap" `Heap ]
-           in
-           frame.scopes <- scopes);
-    Lwt.return ()
-  in
   let execute =
     let temporary_trap_barrier_and_breakpoint = ref None in
     let check_met_temporary_trap_barrier_and_breakpoint report =
@@ -408,9 +267,9 @@ let start agent =
     in
     let wrap_run f () =
       agent.set_status Running;
-      clear_scene agent;
+      Inspect.clear_scene agent.inspect;
       let%lwt report, status = f () in
-      update_scene agent report;%lwt
+      Inspect.update_scene agent.inspect conn report;%lwt
       agent.set_status status;
       Lwt.return ()
     in
@@ -438,7 +297,7 @@ let start agent =
     let step_in = wrap_run internal_step_in in
     let internal_step_out () =
       let promise, resolver = Lwt.task () in
-      exec_with_frame agent conn 1 (fun frame ->
+      Inspect.exec_with_frame agent.inspect conn 1 (fun frame ->
           Lwt.wakeup_later resolver frame;
           Lwt.return ());%lwt
       let%lwt frame = promise in
