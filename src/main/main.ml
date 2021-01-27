f3a98a8c08f100ba4f5023a8454cff13f0b10dbe
文宇祥 <hackwaly@qq.com>
2021-01-28 03:41:27 +0800

    Fixup

--- src/main/main.ml
+++ src/main/main.ml
@@ -32,6 +32,7 @@ let setup_log =
   Term.(const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ())
 
 let debug_command =
+  Logs.set_level None;
   let debug () = Lwt_main.run (debug ()) in
   Term.(const debug $ const (), info "debug")
 
