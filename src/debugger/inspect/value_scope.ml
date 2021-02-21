2870df91689d03b954e2d45cb2c659864caa2de3
文宇祥 <hackwaly@qq.com>
2021-02-21 18:13:10 +0800

    Show accu pseudo variable

--- src/debugger/inspect/value_scope.ml
+++ src/debugger/inspect/value_scope.ml
@@ -29,11 +29,11 @@ class virtual scope_value =
   end
 
 class local_scope_value ~scene ~frame ~kind () =
-  let variables =
+  let variables_and_accu_ty =
     Lazy.from_fun (fun () ->
         match frame.event with
-        | None -> [||]
-        | Some event ->
+        | None -> ([||], None)
+        | Some event -> (
             let typenv = Lazy.force frame.typenv in
             let compenv =
               match kind with
@@ -41,48 +41,60 @@ class local_scope_value ~scene ~frame ~kind () =
               | `Heap -> event.ev_compenv.ce_heap
             in
             let iter f = compenv |> Ident.iter (fun id pos -> f (id, pos)) in
-            Iter.to_list iter
-            |> List.fast_sort (Compare.by (fun (_, pos) -> pos))
-            |> List.to_seq
-            |> Seq.filter_map (fun (id, pos) ->
-                   match typenv |> Typenv.find_value (Path.Pident id) with
-                   | exception Not_found -> None
-                   | { val_type; val_kind; _ } ->
-                       let ty = Ctype.correct_levels val_type in
-                       Some (Ident.name id, val_kind, ty, pos))
-            |> Array.of_seq)
+            ( Iter.to_list iter
+              |> List.fast_sort (Compare.by (fun (_, pos) -> pos))
+              |> List.to_seq
+              |> Seq.filter_map (fun (id, pos) ->
+                     match typenv |> Typenv.find_value (Path.Pident id) with
+                     | exception Not_found -> None
+                     | { val_type; val_kind; _ } ->
+                         let ty = Ctype.correct_levels val_type in
+                         Some (Ident.name id, val_kind, ty, pos))
+              |> Array.of_seq,
+              match event.ev_kind with Event_after ty -> Some ty | _ -> None )))
   in
   object
     inherit scope_value
 
-    method! num_named = Lazy.force variables |> Array.length
+    method! num_named =
+      let variables, accu_ty = Lazy.force variables_and_accu_ty in
+      Array.length variables + if Option.is_some accu_ty then 1 else 0
 
     method! list_named =
       let typenv = Lazy.force frame.typenv in
-      Lazy.force variables |> Array.to_list
-      |> Lwt_list.map_s (fun (id, val_kind, ty, pos) ->
-             let%lwt rv =
-               match kind with
-               | `Stack -> Scene.get_local scene frame pos
-               | `Heap -> Scene.get_environment scene frame pos
-             in
-             match val_kind with
-             | Types.Val_ivar (_, cl_num) -> (
-                 try
-                   let p0, _ =
-                     Typenv.find_value_by_name
-                       (Longident.Lident ("self-" ^ cl_num))
-                       typenv
-                   in
-                   let%lwt v = Eval.value_path scene frame p0 in
-                   let%lwt i = Scene.marshal_obj scene rv in
-                   let%lwt rv' = Scene.get_field scene v i in
-                   let%lwt obj = adopt scene typenv rv' ty in
-                   Lwt.return (id, obj)
-                 with _ -> Lwt.return (id, uninitialized_value) )
-             | _ ->
-                 let%lwt obj = adopt scene typenv rv ty in
-                 Lwt.return (id, obj))
+      let variables, accu_ty = Lazy.force variables_and_accu_ty in
+      let%lwt pairs =
+        variables |> Array.to_list
+        |> Lwt_list.map_s (fun (id, val_kind, ty, pos) ->
+               let%lwt rv =
+                 match kind with
+                 | `Stack -> Scene.get_local scene frame pos
+                 | `Heap -> Scene.get_environment scene frame pos
+               in
+               match val_kind with
+               | Types.Val_ivar (_, cl_num) -> (
+                   try
+                     let p0, _ =
+                       Typenv.find_value_by_name
+                         (Longident.Lident ("self-" ^ cl_num))
+                         typenv
+                     in
+                     let%lwt v = Eval.value_path scene frame p0 in
+                     let%lwt i = Scene.marshal_obj scene rv in
+                     let%lwt rv' = Scene.get_field scene v i in
+                     let%lwt obj = adopt scene typenv rv' ty in
+                     Lwt.return (id, obj)
+                   with _ -> Lwt.return (id, uninitialized_value))
+               | _ ->
+                   let%lwt obj = adopt scene typenv rv ty in
+                   Lwt.return (id, obj))
+      in
+      match accu_ty with
+      | None -> Lwt.return pairs
+      | Some ty ->
+          let%lwt rv' = Scene.get_accu scene frame in
+          let%lwt obj = adopt scene typenv rv' ty in
+          Lwt.return (("%accu", obj) :: pairs)
   end
 
 class global_scope_value ~scene ~frame () =
