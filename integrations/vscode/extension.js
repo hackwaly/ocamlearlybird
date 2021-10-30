f8c588fe01cb7b4057bddd1ac73b287b17afdaa0
cdaringe <cdaringe@gmail.com>
2021-10-29 23:59:48 -0700

    feat: add extension logging

--- integrations/vscode/extension.js
+++ integrations/vscode/extension.js
@@ -1,12 +1,19 @@
-const vscode = require('vscode');
-const FS = require('fs');
-const Path = require('path');
-const CP = require('child_process');
-const Util = require('util');
+const vscode = require("vscode");
+const FS = require("fs");
+const Path = require("path");
+const CP = require("child_process");
+const Util = require("util");
 
-const SUPPORTED_MAGICS = ['Caml1999X028', 'Caml1999X029'];
+const SUPPORTED_MAGICS = ["Caml1999X028", "Caml1999X029"];
 const MAGIC_LENGTH = SUPPORTED_MAGICS[0].length;
 
+const log = (() => {
+  const logger = vscode.window.createOutputChannel();
+  return (text) => {
+    logger.appendLine(text);
+    logger.show();
+  };
+})();
 
 const isBytecodeFile = async (path) => {
   try {
@@ -15,7 +22,7 @@ const isBytecodeFile = async (path) => {
     try {
       const buffer = Buffer.alloc(MAGIC_LENGTH);
       await file.read(buffer, 0, MAGIC_LENGTH, stat.size - MAGIC_LENGTH);
-      const magic = buffer.toString('ascii');
+      const magic = buffer.toString("ascii");
       return SUPPORTED_MAGICS.includes(magic);
     } finally {
       file.close();
@@ -27,74 +34,114 @@ const isBytecodeFile = async (path) => {
 
 const debugConfigProvider = {
   async provideDebugConfigurations(folder, token) {
-    return [{
-      name: 'OCaml Debug',
-      type: 'ocamlearlybird',
-      request: 'launch',
-      program: '${workspaceFolder}/a.out',
-      stopOnEntry: false,
-      yieldSteps: 4096,
-      onlyDebugGlob: "<${workspaceFolder}/**/*>"
-    }];
+    return [
+      {
+        name: "OCaml Debug",
+        type: "ocamlearlybird",
+        request: "launch",
+        program: "${workspaceFolder}/a.out",
+        stopOnEntry: false,
+        yieldSteps: 4096,
+        onlyDebugGlob: "<${workspaceFolder}/**/*>",
+      },
+    ];
   },
   async resolveDebugConfiguration(folder, config, token) {
     if (!config.type) {
       config = {
-        name: '${fileBasename}',
-        type: 'ocamlearlybird',
-        request: 'launch',
-        program: '${file}',
+        name: "${fileBasename}",
+        type: "ocamlearlybird",
+        request: "launch",
+        program: "${file}",
       };
     }
     return config;
   },
-  async resolveDebugConfigurationWithSubstitutedVariables(folder, config, token) {
-    if (!await isBytecodeFile(config.program)) {
-      vscode.window.showErrorMessage('Unsupported file format');
+  async resolveDebugConfigurationWithSubstitutedVariables(
+    folder,
+    config,
+    token
+  ) {
+    if (!(await isBytecodeFile(config.program))) {
+      vscode.window.showErrorMessage("Unsupported file format");
       return;
     }
     return config;
-  }
+  },
 };
+
+const jsonStringifyReplacer = (k, v) =>
+  k ? (typeof v === "function" ? `function:${v.name || k}` : "function") : v;
+const jsonStringify = (str) => JSON.stringify(str, jsonStringifyReplacer, 2);
 
 module.exports = {
   /**@param {vscode.ExtensionContext} context */
   activate(context) {
-    const config = vscode.workspace.getConfiguration('ocamlearlybird');
-    const ocamlearlybirdPath = config.get('path');
-    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('ocamlearlybird', {
-      createDebugAdapterDescriptor(session, executable) {
-        return new vscode.DebugAdapterExecutable(ocamlearlybirdPath, ['debug']);
-      }
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('ocamlearlybird.startDebug', async (uri) => {
-      const folder = vscode.workspace.getWorkspaceFolder(uri);
-      await vscode.debug.startDebugging(folder, {
-        name: Path.basename(uri.fsPath),
-        type: "ocamlearlybird",
-        request: "launch",
-        stopOnEntry: true,
-        yieldSteps: 4096,
-        program: uri.fsPath,
-      });
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('ocamlearlybird.variableGotoClosureCodeLocation', async (context) => {
-      const result = await vscode.debug.activeDebugSession.customRequest("variableGetClosureCodeLocation", { handle: context.variable.variablesReference });
-      if (result.location != null) {
-        const loc = result.location;
-        const doc = vscode.workspace.openTextDocument(result.location.source);
-        vscode.window.showTextDocument(doc, {
-          preview: true,
-          selection: new vscode.Range(
-            new vscode.Position(loc.pos[0] - 1, loc.pos[1] - 1),
-            new vscode.Position(loc.end_[0] - 1, loc.end_[1] - 1),
-          ),
+    log(`activating`);
+    const config = vscode.workspace.getConfiguration("ocamlearlybird");
+    const serializedConfig = jsonStringify(config);
+    log(`configuration detected: ${serializedConfig}`);
+    const ocamlearlybirdPath = config.get("path");
+    context.subscriptions.push(
+      vscode.debug.registerDebugAdapterDescriptorFactory("ocamlearlybird", {
+        createDebugAdapterDescriptor(session, executable) {
+          log(`on:createDebugAdapterDescriptor`);
+          return new vscode.DebugAdapterExecutable(ocamlearlybirdPath, [
+            "debug",
+          ]);
+        },
+      })
+    );
+    context.subscriptions.push(
+      vscode.commands.registerCommand("ocamlearlybird.startDebug", async () => {
+        log(`on:ocamlearlybird.startDebug`);
+        const uri = vscode.window.activeTextEditor.document.uri;
+        const folder = vscode.workspace.getWorkspaceFolder();
+        await vscode.debug.startDebugging(folder, {
+          name: Path.basename(uri.fsPath),
+          type: "ocamlearlybird",
+          request: "launch",
+          stopOnEntry: true,
+          yieldSteps: 4096,
+          program: uri.fsPath,
         });
-      } else {
-        vscode.window.showInformationMessage("No closure code location");
-      }
-    }));
-    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('ocamlearlybird', debugConfigProvider));
+      })
+    );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        "ocamlearlybird.variableGotoClosureCodeLocation",
+        async (context) => {
+          log(`on:ocamlearlybird.variableGotoClosureCodeLocation`);
+          const result = await vscode.debug.activeDebugSession.customRequest(
+            "variableGetClosureCodeLocation",
+            { handle: context.variable.variablesReference }
+          );
+          if (result.location != null) {
+            const loc = result.location;
+            const doc = vscode.workspace.openTextDocument(
+              result.location.source
+            );
+            vscode.window.showTextDocument(doc, {
+              preview: true,
+              selection: new vscode.Range(
+                new vscode.Position(loc.pos[0] - 1, loc.pos[1] - 1),
+                new vscode.Position(loc.end_[0] - 1, loc.end_[1] - 1)
+              ),
+            });
+          } else {
+            vscode.window.showInformationMessage("No closure code location");
+          }
+        }
+      )
+    );
+    context.subscriptions.push(
+      vscode.debug.registerDebugConfigurationProvider(
+        "ocamlearlybird",
+        debugConfigProvider
+      )
+    );
   },
-  deactivate() { }
+  deactivate() {
+    log("deactivated");
+  },
 };
