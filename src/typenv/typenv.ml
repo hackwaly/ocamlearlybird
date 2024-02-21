50a4a5ac64ba79d6e4346190162d4530b1323a67
Simmo Saan <simmo.saan@gmail.com>
2024-02-21 17:37:50 +0200

    Add Persistent_env.Persistent_signature.load support in OCaml 5.2

--- src/typenv/typenv.ml
+++ src/typenv/typenv.ml
@@ -6,6 +6,15 @@ let load_path_init = Load_path.init ~auto_include:Load_path.no_auto_include
 let load_path_init = Load_path.init
 [%%endif]
 
+[%%if ocaml_version >= (5, 2, 0)]
+let () =
+  let old_load = !Persistent_env.Persistent_signature.load in
+  Persistent_env.Persistent_signature.load := (fun ~allow_hidden ~unit_name ->
+      let search_dirs = !persistent_env_get_search_dirs unit_name in
+      load_path_init search_dirs;
+      old_load ~allow_hidden ~unit_name
+    )
+[%%else]
 let () =
   let old_load = !Persistent_env.Persistent_signature.load in
   Persistent_env.Persistent_signature.load := (fun ~unit_name ->
@@ -13,6 +22,7 @@ let () =
       load_path_init search_dirs;
       old_load ~unit_name
     )
+[%%endif]
 
 let env_extract_values path env =
   Env.fold_values (fun name _ _ acc -> name :: acc) path env []
