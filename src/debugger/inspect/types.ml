1d3e292ef9769d6bb27b11964b8772fc35cbdcc7
Simmo Saan <simmo.saan@gmail.com>
2025-08-20 11:33:55 +0300

    Add Data_types support in OCaml 5.4

--- src/debugger/inspect/types.ml
+++ src/debugger/inspect/types.ml
@@ -6,3 +6,11 @@ let commu_ok = Cok
 let row_fields row = row.row_fields
 let row_field_repr = Btype.row_field_repr
 [%%endif]
+
+[%%if ocaml_version >= (5, 4, 0)]
+type constructor_tag = Data_types.constructor_tag =
+  | Cstr_constant of int
+  | Cstr_block of int
+  | Cstr_unboxed
+  | Cstr_extension of Path.t * bool
+[%%endif]
