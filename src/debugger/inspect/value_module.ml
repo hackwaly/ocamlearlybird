050d30c36bee241d14b1ced0fa686dc3f85afdf8
Simmo Saan <simmo.saan@gmail.com>
2025-08-20 11:34:41 +0300

    Add Tpackage record support in OCaml 5.4

--- src/debugger/inspect/value_module.ml
+++ src/debugger/inspect/value_module.ml
@@ -78,7 +78,19 @@ let adopter scene typenv obj typ =
         Lwt.return
           (Some (new module_value ~scene ~typenv:typenv' ~obj ~path:(Path.Pident mid) ()))
       with _ -> Lwt.return (Some unknown_module_value) )
-  | Tpackage (path, []) [@if ocaml_version >= (4, 13, 0)] -> (
+  | Tpackage (path, []) [@if ocaml_version >= (4, 13, 0) && ocaml_version < (5, 4, 0)] -> (
+      try
+        let mty = typenv |> Typenv.find_modtype_expansion path in
+        let mid =
+          Ident.create_persistent
+            (Printf.sprintf "M_%04x"
+                (Float.to_int (Sys.time () *. 1e9) mod 0x10000))
+        in
+        let typenv' = typenv |> Typenv.add_module mid Types.Mp_present mty in
+        Lwt.return
+          (Some (new module_value ~scene ~typenv:typenv' ~obj ~path:(Path.Pident mid) ()))
+      with _ -> Lwt.return (Some unknown_module_value) )
+  | Tpackage {pack_path = path; pack_cstrs = []} [@if ocaml_version >= (5, 4, 0)] -> (
       try
         let mty = typenv |> Typenv.find_modtype_expansion path in
         let mid =
